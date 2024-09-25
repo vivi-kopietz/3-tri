b460a95eb6be26f3cbfc9e3aaf73f36593494571
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-09-25 08:27:45 -0300

    gf

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ console.log(soma(5,3))
 
 function areaQuadrado(lado){
     return lado**2
-} console.log(areaQuadrado(6))
+} 
+console.log(areaQuadrado(6))
+let media = function(numero1,numero2,numero3)
+{return numero1+numero2+numero3
+}
+console.log(media(1,2,3))
 
 
