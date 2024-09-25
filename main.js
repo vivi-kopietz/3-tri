94648ace7cf434a1e61a298b586f73328d8d2629
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-09-25 08:28:48 -0300

    erer

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function areaQuadrado(lado){
 } 
 console.log(areaQuadrado(6))
 let media = function(numero1,numero2,numero3)
-{return numero1+numero2+numero3
+{return numero1+numero2+numero3/3
 }
 console.log(media(1,2,3))
 
