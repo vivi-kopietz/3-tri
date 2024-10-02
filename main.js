c63e16d4446326f52df1e79da895e352e3acbe4b
Viviane Kopietz <142537081+vivi-kopietz@users.noreply.github.com>
2024-10-02 08:04:35 -0300

    çç

--- main.js
+++ main.js
@@ -16,8 +16,9 @@ console.log(media(1,2,3))
 let estados=["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"];
 let i=0;27
 while(i<27)
-     {console.log(i);
+ {console.log(i);
 console.log(estados[i]),
+
 
         i=i+1
 
